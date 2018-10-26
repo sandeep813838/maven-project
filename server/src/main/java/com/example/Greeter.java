1a6f74da0cc073bffeee40c1bffa6ebddf865b7d
sampath <Jenkins@server.com>
2018-10-26 21:58:27 +0530

    error corrected in greeter.java file 2nd

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
   /**
-   * this greenting string
+   * @param this greenting string.
+   * @return greeting string.
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
